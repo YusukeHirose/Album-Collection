4c5a2d6cb9f0565db3c2b81b29d9a3a8fde9397b
kdoebr21 <kodeberic@yahoo.co.jp>
2017-12-13 13:42:34 +0900

    first commit

---
+++ Album-Collection/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Album-Collection
+//
+//  Created by User on 2017/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
